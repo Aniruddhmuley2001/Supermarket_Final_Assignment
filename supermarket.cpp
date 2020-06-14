7c6a04631c922a4128295300b1048585df1f503c
Aniruddhmuley2001 <aniruddhmuley2001@yahoo.com>
2020-06-14 18:07:42 +0530

    temporarily class created

--- supermarket.cpp
+++ supermarket.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+class Inventory{
+	
+}
+
+class Customer{
+	
+}
+
+class Manager{
+	
+}
+
+class Billing{
+	
+}
+
 int main(){
 	cout<<"Hello World!";
 }
